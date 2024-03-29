3ea280281dfc0d532ff608b61f491188fba975c7
Denny_Kate <dennykate22@gmail.com>
2024-03-30 00:25:57 +0630

    feat: add meta tag

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,11 +3,7 @@ import { Link } from "react-router-dom";
 const Logo = () => {
   return (
     <Link to="/" className="flex items-center gap-2">
-      <img
-        src={"https://i.postimg.cc/8z2k1W3p/tech-logo.jpg"}
-        alt="logo"
-        className="h-[50px] object-cover"
-      />
+      <img src={"/logo.svg"} alt="logo" className="h-[50px] object-cover" />
       <p className="text-base">Galaxy</p>
     </Link>
   );
