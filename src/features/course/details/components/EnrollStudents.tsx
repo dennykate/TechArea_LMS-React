f7df17d91d9112bc738ecfc0a1dc45147c925b41
Denny_Kate <dennykate22@gmail.com>
2024-03-31 00:02:09 +0630

    feat: remove table spacing

--- src/features/course/details/components/EnrollStudents.tsx
+++ src/features/course/details/components/EnrollStudents.tsx
@@ -18,28 +18,20 @@ const EnrollStudents = () => {
       [0, 1]?.map((element: any, i: number) => (
         <tr key={i}>
           <td className="m_td">1</td>
-
           <td className="m_td">
             <Avatar
               size="lg"
               src="https://images.pexels.com/photos/3775087/pexels-photo-3775087.jpeg?auto=compress&cs=tinysrgb&w=600"
             />
           </td>
-
           <td className="m_td">Denny Kate</td>
-
           <td className="m_td">
             <a href={`tel:09964470356`}>09964470356</a>
           </td>
-
           <td className="m_td">Male</td>
-
           <td className="m_td">01 Dec 2000</td>
-
           <td className="m_td">Ma Ma</td>
-
           <td className="m_td">22 March 2024</td>
-          
           <td className="m_td">
             <TableActions
               detailCb={() => navigate("/accounts/students/details/1")}
