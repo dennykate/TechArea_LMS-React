7d30bd98629e1cfa5922aeb979e6c836059f9355
dennykate <dennykate22@gmail.com>
2024-05-11 21:57:00 +0630

    faet: commit - 3

--- src/features/quiz/details/components/AnswerStudents.tsx
+++ src/features/quiz/details/components/AnswerStudents.tsx
@@ -23,8 +23,9 @@ const AnswerStudents = () => {
           <td className="m_td">{element?.name}</td>
 
           <td className="m_td">{element?.gender}</td>
-          
+
           <td className="m_td">{element?.grade}</td>
+          
           <td className="m_td">{element?.section}</td>
           <td className="m_td">{element?.total_score}</td>
           <td className="m_td">{element?.total_score_percentage}</td>
