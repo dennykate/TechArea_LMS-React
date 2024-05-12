5b2df81ab57798dd45efcbbf38fbbfa9f285f3cc
dennykate <dennykate22@gmail.com>
2024-05-12 22:18:23 +0630

    feat: commit - 11

--- src/features/landing/components/EventCard.tsx
+++ src/features/landing/components/EventCard.tsx
@@ -50,6 +50,7 @@ const EventCard: React.FC<PropsType> = ({ data }) => {
           <div
           
             dangerouslySetInnerHTML={{ __html: data?.description }}
+            
             className="font-sans text-base mt-2 line-clamp-3"
           />
         </div>
